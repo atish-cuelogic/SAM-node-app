a544fbcd51d533f400de018c337c16a871eaf2c7
Atish SAhoo <atishkumar.sahoo@cuelogic.com>
2018-02-08 11:27:02 +0000

    SNS subscription set

--- src/publisher.js
+++ src/publisher.js
@@ -1,6 +1,16 @@
 'use strict';
 
+var AWS = require("aws-sdk");
+
 exports.handler = (event, context, callback) => {
+
+	var sns = new AWS.SNS();
+    var params = {
+        Message: "eventText testing", 
+        Subject: "Test SNS From Lambda",
+        TopicArn: process.env.topicARN
+    };
+    sns.publish(params, context.done);
 
     callback(null, {
         statusCode: '200',
